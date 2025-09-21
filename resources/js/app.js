432adb0c08c8b6742660e4e1a7c838738186ab29
dionemourao <silvadevbr@gmail.com>
2025-09-20 21:54:09 -0300

    20/09

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+
+
+import { createApp } from 'vue';
+
+const app = createApp();
+
+
+app.component('login', Login);
+
+app.amount('#app');
+
---
+++ resources/views/v1/login.blade.php
@@ -0,0 +1,6 @@
+@extends('v1/_layouts/main')
+
+
+@section('content')
+    <h2>Login</h2>
+@endsection
